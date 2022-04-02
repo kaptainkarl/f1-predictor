2616488bfa68bcf4334c6bd13a6fe845713cd9ca
kaptainkarl <karlauto70@gmail.com>
2022-04-03 00:01:40 +0100

    tlb gen script, plus a bit of disabling in the push to github script.

--- generate-both-n-push.bash
+++ generate-both-n-push.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-git rm output/2022/favourites/*
-git rm output/2022/all-algorithms/*
+git rm -r output/2022/*
+
+mkdir -p output/2022/favourites/
+mkdir -p output/2022/all-algorithms/
+mkdir -p output/2022/tlb/
+mkdir -p output/2022/tlb-totals/
+mkdir -p output/2022/json/
+mkdir -p output/2022/csv/
 
 ./generate-all-types.pl
 ./generate-fav-types.pl
+./generate-tlb-types.pl
 
-git add output/2022/favourites/
-git add output/2022/all-algorithms/
-
-git commit -m "generated output"
-
-git push github
-
-
+#git add output/2022/
+#git commit -m "generated output"
+#git push github
 
---
+++ generate-tlb-types.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;use warnings;
+
+# for running all the races and putting the output in the output dir.
+
+my $runs = {
+    RACES => "bahrain-race,jeddah-race",
+    QUAL  => "bahrain-qual,jeddah-qual",
+    ALL   => "bahrain-qual,bahrain-race,jeddah-qual,jeddah-race",
+};
+
+my $score_sys = [
+    "karl-8",
+    "karl-32",
+    "diff",
+    "exact",
+];
+
+my $multipliers = [
+    " --score-times-current ",
+    " --score-times-1990 ",
+    " --score-times-power-100 ",
+    "", # no multiplier
+];
+
+#./f1-predictor.pl --run bahrain-qual,jeddah-qual   --no-pre-code --out-file "blah"
+
+for my $sc ( @$score_sys ) {
+    for my $ml ( @$multipliers ){
+        for my $r (keys %$runs){
+            my $cmd = "./f1-predictor.pl --player-rating-score ";
+
+            $cmd .= " --out-sub-dir tlb ";
+
+            $cmd .= " --score-sys $sc $ml --out-file $r --run $runs->{$r}\n";
+
+            system( $cmd ) ;
+        }
+    }
+}
