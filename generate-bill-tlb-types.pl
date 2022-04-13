fa41a4e609c49bcb1c0f777137666d67a795d117
kaptainkarl <karlauto70@gmail.com>
2022-04-13 23:58:59 +0100

    update a couple of generate scripts.

--- generate-bill-tlb-types.pl
+++ generate-bill-tlb-types.pl
@@ -8,50 +8,62 @@ use GenerateRaces;
 my $runs = $GenerateRaces::RUNS;
 
 for my $r (keys %$runs){
-    my $cmd = "./f1-predictor.pl ";
+    my $cmd = "./f1-predictor.pl --out-file $r";
     $cmd .= " --out-sub-dir bill-tlb ";
-    $cmd .= " --no-pre-code";
-    #$cmd .= " --no-pos-col";
-    $cmd .= " --out-file $r";
 
+    # $cmd .= " --player-rating-score ";
+    $cmd .= " --no-pre-code";
+
+    #$cmd .= " --no-pos-col";
     $cmd .= " --separator '' ";
+
     $cmd .= " --no-detail ";
     $cmd .= " --case-change-not-exact-predictions ";
-    $cmd .= " --fia --fia-simple ";
-    $cmd .= " --wta ";
-    $cmd .= " --run $runs->{$r}\n";
 
+    #$cmd .= " --fia ";
+    $cmd .= " --fia-simple ";
+
+    $cmd .= " --wta --run $runs->{$r}\n";
     system( $cmd ) ;
 }
 
 for my $r (keys %$runs){
-    my $cmd = "./f1-predictor.pl ";
-    # $cmd .= " --player-rating-score ";
+    my $cmd = "./f1-predictor.pl --out-file $r";
     $cmd .= " --out-sub-dir bill-tlb-detailed ";
+
+    # $cmd .= " --player-rating-score ";
     $cmd .= " --no-pre-code";
-    $cmd .= " --no-pos-col";
-    $cmd .= " --out-file $r";
-    # $cmd .= " --separator ', ' ";
 
-    $cmd .= " --fia --fia-simple ";
-    $cmd .= " --wta ";
-    $cmd .= " --run $runs->{$r}\n";
+    #$cmd .= " --no-pos-col";
+    #$cmd .= " --separator ', ' ";
 
+    #$cmd .= " --no-detail ";
+    #$cmd .= " --case-change-not-exact-predictions ";
+
+    $cmd .= " --fia ";
+    $cmd .= " --fia-simple ";
+
+    $cmd .= " --wta --run $runs->{$r}\n";
     system( $cmd ) ;
 }
 
 for my $r (keys %$runs){
-    my $cmd = "./f1-predictor.pl ";
-    $cmd .= " --player-rating-score ";
+    my $cmd = "./f1-predictor.pl --out-file $r";
     $cmd .= " --out-sub-dir bill-tlb-very-detailed ";
+
+    $cmd .= " --player-rating-score ";
     $cmd .= " --no-pre-code";
-    $cmd .= " --no-pos-col";
-    $cmd .= " --out-file $r";
-    # $cmd .= " --separator ', ' ";
 
-    $cmd .= " --fia --fia-simple ";
-    $cmd .= " --wta ";
-    $cmd .= " --run $runs->{$r}\n";
+    #$cmd .= " --no-pos-col";
+    #$cmd .= " --separator ', ' ";
 
+    #$cmd .= " --no-detail ";
+    #$cmd .= " --case-change-not-exact-predictions ";
+
+    $cmd .= " --fia ";
+    $cmd .= " --fia-simple ";
+    $cmd .= " --show-p1-to-p6-totals";
+
+    $cmd .= " --wta --run $runs->{$r}\n";
     system( $cmd ) ;
 }
