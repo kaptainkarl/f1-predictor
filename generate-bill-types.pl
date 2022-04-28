bb65d61334e3a88baa330317ca0396970b33649c
kaptainkarl <karlauto70@gmail.com>
2022-04-28 21:42:33 +0100

    fixes to bill generate script.

--- generate-bill-types.pl
+++ generate-bill-types.pl
@@ -5,10 +5,20 @@ use FindBin;
 use lib "$FindBin::RealBin/.";
 use GenerateRaces;
 my $runs = $GenerateRaces::RUNS;
+
+# some global option twiddling on all outputs.
+my $o_method = " --closest-p1-20 ";
+my $o_fia_sprint_qual_diff = " --fia-sprint-qual-diff ";
+# $o_fia_sprint_qual_diff = "";
+my $only_races = 1;
+
+
 for my $r (keys %$runs){
-    next if $r ne "RACES";
+    next if $r ne "RACES" && $only_races;
     my $cmd = "./f1-predictor.pl ";
     $cmd .= " --out-sub-dir bill ";
+    $cmd .= " $o_method ";
+    $cmd .= " $o_fia_sprint_qual_diff ";
     $cmd .= " --bill";
     $cmd .= " --out-file $r";
     $cmd .= " --run $runs->{$r}";
@@ -16,14 +26,13 @@ for my $r (keys %$runs){
     $cmd .= " --html-out ";
     print "$cmd\n";
     system( $cmd );
-}
 
-for my $r (keys %$runs){
-    next if $r ne "RACES";
-    my $cmd = "./f1-predictor.pl ";
+
+    $cmd = "./f1-predictor.pl ";
     $cmd .= " --out-sub-dir bill ";
-    $cmd .= " --closest-p1-20 ";
-    $cmd .= " --fia --fia-sprint-qual-diff ";
+    $cmd .= " $o_method ";
+    $cmd .= " --fia ";
+    $cmd .= " $o_fia_sprint_qual_diff ";
     $cmd .= " --suppress-average-table ";
     $cmd .= " --no-pre-code ";
     $cmd .= " --out-file $r-DETAILED ";
@@ -31,14 +40,13 @@ for my $r (keys %$runs){
     $cmd .= " --html-out ";
     print "$cmd\n";
     system( $cmd );
-}
 
-for my $r (keys %$runs){
-    next if $r ne "RACES";
-    my $cmd = "./f1-predictor.pl ";
+
+    $cmd = "./f1-predictor.pl ";
     $cmd .= " --out-sub-dir bill ";
-    $cmd .= " --closest-p1-20 ";
-    $cmd .= " --fia --fia-sprint-qual-diff ";
+    $cmd .= " $o_method ";
+    $cmd .= " --fia ";
+    $cmd .= " $o_fia_sprint_qual_diff ";
     $cmd .= " --suppress-average-table ";
     $cmd .= " --no-pre-code ";
     $cmd .= " --out-file $r-DETAILED-RATING ";
@@ -48,14 +56,13 @@ for my $r (keys %$runs){
     $cmd .= " --html-out ";
     print "$cmd\n";
     system( $cmd );
-}
 
-for my $r (keys %$runs){
-    next if $r ne "RACES";
-    my $cmd = "./f1-predictor.pl ";
+
+    $cmd = "./f1-predictor.pl ";
     $cmd .= " --out-sub-dir bill ";
-    $cmd .= " --closest-p1-20 ";
-    $cmd .= " --fia --fia-sprint-qual-diff ";
+    $cmd .= " $o_method ";
+    $cmd .= " --fia ";
+    $cmd .= " $o_fia_sprint_qual_diff ";
     $cmd .= " --suppress-average-table ";
     $cmd .= " --no-pre-code ";
     $cmd .= " --out-file $r-TOO-MUCH-DETAIL ";
