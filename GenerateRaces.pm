9ca20c5aba6275a93b8245cc8a7170ed07354b96
kaptainkarl <karlauto70@gmail.com>
2022-05-08 00:08:24 +0100

    add miami-qual to the GenerateRaces.pm

--- GenerateRaces.pm
+++ GenerateRaces.pm
@@ -2,8 +2,8 @@ package GenerateRaces;
 use strict; use warnings;
 
 our $RUNS =  {
-    ALL   => "enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
-    QUAL  => "enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
+    ALL   => "miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
+    QUAL  => "miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
     RACES => "enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
 };
 
