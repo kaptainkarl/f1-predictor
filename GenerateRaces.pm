466ffa8d1e8212d91e9df20f3b824bf74b27b48a
kaptainkarl <karlauto70@gmail.com>
2022-05-22 16:13:22 +0100

    post spain race update.

--- GenerateRaces.pm
+++ GenerateRaces.pm
@@ -2,9 +2,9 @@ package GenerateRaces;
 use strict; use warnings;
 
 our $RUNS =  {
-    ALL   => "spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
+    ALL   => "spain-race,spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
     QUAL  => "spain-qual,miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
-    RACES => "miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
+    RACES => "spain-race,miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
 };
 
 1;
