b2abd1fefbc98031e699b878442f7b146a71ce2a
kaptainkarl <karlauto70@gmail.com>
2022-05-29 22:14:04 +0100

    Post Monaco update.

--- GenerateRaces.pm
+++ GenerateRaces.pm
@@ -1,10 +1,14 @@
 package GenerateRaces;
 use strict; use warnings;
 
+
 our $RUNS =  {
-    ALL   => "spain-race,spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
-    QUAL  => "spain-qual,miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
-    RACES => "spain-race,miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
+    ALL   => "monaco-race,monaco-qual,spain-race,spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
+    QUAL  => "monaco-qual,spain-qual,miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
+    RACES => "monaco-race,spain-race,miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
+    BOT_6_RACES => "monaco-bottom-6-race",
+    BOT_6_QUAL  => "monaco-bottom-6-qual",
+    BOT_6_ALL   => "monaco-bottom-6-race,monaco-bottom-6-qual",
 };
 
 1;
