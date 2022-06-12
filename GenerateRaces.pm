9f8069ae277bb1e072cdccd80e2c1dafbda32a9b
kaptainkarl <karlauto70@gmail.com>
2022-06-12 14:07:34 +0100

    baku update

--- GenerateRaces.pm
+++ GenerateRaces.pm
@@ -3,12 +3,14 @@ use strict; use warnings;
 
 
 our $RUNS =  {
-    ALL   => "monaco-race,monaco-qual,spain-race,spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
-    QUAL  => "monaco-qual,spain-qual,miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
-    RACES => "monaco-race,spain-race,miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
-    BOT_6_RACES => "monaco-bottom-6-race",
-    BOT_6_QUAL  => "monaco-bottom-6-qual",
-    BOT_6_ALL   => "monaco-bottom-6-race,monaco-bottom-6-qual",
+    ALL   => "baku-race,baku-qual,monaco-race,monaco-qual,spain-race,spain-qual,miami-race,miami-qual,enzo-race,enzo-sprint,enzo-qual,australia-race,australia-qual,jeddah-race,jeddah-qual,bahrain-race,bahrain-qual",
+
+    QUAL  => "baku-qual,monaco-qual,spain-qual,miami-qual,enzo-qual,australia-qual,jeddah-qual,bahrain-qual",
+    RACES => "baku-race,monaco-race,spain-race,miami-race,enzo-race,enzo-sprint,australia-race,jeddah-race,bahrain-race",
+
+    BOT_6_RACES => "baku-bottom-6-race,monaco-bottom-6-race",
+    BOT_6_QUAL  => "baku-bottom-6-qual,monaco-bottom-6-qual",
+    BOT_6_ALL   => "baku-bottom-6-race,baku-bottom-6-qual,monaco-bottom-6-race,monaco-bottom-6-qual",
 };
 
 1;
